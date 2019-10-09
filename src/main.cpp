df2f0154208d1228ded206efa6d07140ce2ee1e1
Nick Nissen <nickknissen@gmail.com>
2019-10-09 21:28:38 +0200

    move config out off main

--- src/main.cpp
+++ src/main.cpp
@@ -17,26 +17,6 @@ extern "C" {
 #include "fd_forward.h"
 #include "fr_forward.h"
 
-#define DEGUB_ESP
-
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    15
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
-
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM        5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       35
-#define Y2_GPIO_NUM       17
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
-
 // Connection timeout;
 #define CON_TIMEOUT   10*1000 // milliseconds
 
@@ -68,10 +48,10 @@ bool take_picture(void);
 
 
 void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);        
+    strip.show();                          
+    delay(wait);
   }
 }
 
